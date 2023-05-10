13975b1c46fb91e512c4c4f4173654e0817cc541
Maximilian Reichel <info@phramz.com>
2023-05-10 15:29:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# LogX - Contract
+
+Unified logging with Golang
+
+## Install
+
+```shell
+go get -u github.com/logx-go/contract
+```
+
+## Development
+
+### Requirement
+- Golang >=1.20
+- golangci-lint (https://golangci-lint.run/)
+
+### Tests
+
+```shell
+go test ./... -race
+```
+
+### Lint
+
+```shell
+golangci-lint run
+```
+
+## License
+
+MIT License (see [LICENSE](LICENSE) file)
+
---
+++ pkg/logx/logx.go
@@ -0,0 +1,71 @@
+package logx
+
+// Adapter interface for logger interoperability
+type Adapter interface {
+	Logger
+	WithFormatter(formatter Formatter) Adapter
+}
+
+// Formatter interface for log output formatting
+type Formatter interface {
+	Format(message *string, fields map[string]any) any
+}
+
+// Log interface for log compliance (https://pkg.go.dev/log)
+type Log interface {
+	// Fatal is equivalent to l.Print() followed by a call to os.Exit(1).
+	Fatal(v ...any)
+	// Panic is equivalent to l.Print() followed by a call to panic().
+	Panic(v ...any)
+	// Print to the logx. Arguments are handled in the manner of fmt.Print.
+	Print(v ...any)
+}
+
+// LogF interface for formatted log compliance (https://pkg.go.dev/log)
+type LogF interface {
+	// Fatalf is equivalent to l.Printf() followed by a call to os.Exit(1).
+	Fatalf(format string, v ...any)
+	// Panicf is equivalent to l.Printf() followed by a call to panic().
+	Panicf(format string, v ...any)
+	// Printf to the logx. Arguments are handled in the manner of fmt.Printf.
+	Printf(format string, v ...any)
+}
+
+// Logger abstraction for structured/field logging
+type Logger interface {
+	Log
+	LogF
+	Debug(v ...any)
+	Debugf(format string, v ...any)
+	Info(v ...any)
+	Infof(format string, v ...any)
+	Notice(v ...any)
+	Noticef(format string, v ...any)
+	Warning(v ...any)
+	Warningf(format string, v ...any)
+	Error(v ...any)
+	Errorf(format string, v ...any)
+}
+
+// log level values
+const (
+	LogLevelDebug   int = 100
+	LogLevelInfo    int = 200
+	LogLevelNotice  int = 300
+	LogLevelWarning int = 400
+	LogLevelError   int = 500
+	LogLevelFatal   int = 600
+	LogLevelPanic   int = 700
+)
+
+// special attribute names
+const (
+	FieldNameLogLevel     string = "level"
+	FieldNameMessage      string = "message"
+	FieldNameTimestamp    string = "timestamp"
+	FieldNameCallerFunc   string = "caller:func"
+	FieldNameCallerFile   string = "caller:file"
+	FieldNameCallerLine   string = "caller:line"
+	FieldNameHTTPRequest  string = "http:request"
+	FieldNameHTTPResponse string = "http:response"
+)
